e749cda2e5ae2827f40f1c2e5b8ecbfd4e1bfe88
BaoDev <giabao7112004@gmail.com>
2025-05-23 15:39:13 +0700

    change database cloud

--- sblog_api/settings.py
+++ sblog_api/settings.py
@@ -85,37 +85,38 @@ WSGI_APPLICATION = 'sblog_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "blog_db",
-#         "USER": "root",
-#         "PASSWORD": "giabao123123",
-#         "HOST": "127.0.0.1",
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-#         }
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blog',
-        'USER': '3SGCqqWGXZXw4R3.root',
-        'PASSWORD': 'lULfjt2svJiGuvJ7',
-        'HOST': 'gateway01.eu-central-1.prod.aws.tidbcloud.com',
-        'PORT': '4000',
-        'OPTIONS': {
-            'charset': 'utf8mb4',  # Add this for proper UTF-8 support
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'ssl': {
-        # tắt verify, cực kỳ không an toàn, chỉ dev test
-                'ssl_disabled': False
-            }
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "railway",
+        "USER": "root",
+        "PASSWORD": "RDpKWlvHMuxtPcsarSjjzUgBqdUqcwLd",
+        "HOST": "tramway.proxy.rlwy.net",  # hoặc mysql-production-fd28.up.railway.app
+        "PORT": "13620",  # Railway mở port này để public connect
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'"
         }
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'blog',
+#         'USER': '3SGCqqWGXZXw4R3.root',
+#         'PASSWORD': 'lULfjt2svJiGuvJ7',
+#         'HOST': 'gateway01.eu-central-1.prod.aws.tidbcloud.com',
+#         'PORT': '4000',
+#         'OPTIONS': {
+#             'charset': 'utf8mb4',  # Add this for proper UTF-8 support
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#             'ssl': {
+#         # tắt verify, cực kỳ không an toàn, chỉ dev test
+#                 'ssl_disabled': False
+#             }
+#         }
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
